a52e3f77304ee67e4657693329447549529a0c49
Charlie Davis <charliedavis@Charlies-MacBook-Pro-2.local>
2019-11-19 22:13:43 -0500

    basic ui framework with fake data

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="navigation">
+		      <div><h3 class="logo">News<span>ic</span></h3></div>
+	      </div>
+        <div class="container">
+          <div class="newsFeedWrapper">
+            <div class="titleWrapper"><h3>News Feed</h3></div>
+            <div class="newsContent">
+              <div class="spacer"></div>
+              <div class="newsArticle">
+                <img class="newsImage" src="http://via.placeholder.com/640x360"/>
+                <div class="newsDetail"></div>
+              </div>
+              <div class="newsArticle"></div>
+            </div>
+          </div>
+          <div class="topArtistWrapper">
+            <div class="titleWrapper" id="artistsTitleWrapper"><h3 class="favoriteArtists">Artists</h3></div>
+            <div class="list-group">
+              <div class="list-group-item"></div>
+              <div class="list-group-item"></div>
+              <div class="list-group-item"></div>
+              <div class="list-group-item"></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
